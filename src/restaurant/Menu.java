53c2842163fc307990bf67f02d6aa8c4e36e281a
jpgfour <jeremy.grise@gmail.com>
2020-05-04 18:16:28 -0500

    Prepping for Class 4 Studio

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -5,15 +5,31 @@ import java.util.Date;
 
 public class Menu {
 
-    private ArrayList<MenuItem> dishes;
+    private ArrayList<MenuItem> menuItems;
     private Date dateLastUpdated;
 
     //constructors
-    public Menu(ArrayList<MenuItem> dishes) {
-        this.dishes = dishes;
+    public Menu(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
     }
-    public Menu(ArrayList<MenuItem> dishes, Date dateLastUpdated) {
-        this.dishes = dishes;
+    public Menu(ArrayList<MenuItem> menuItems, Date dateLastUpdated) {
+        this.menuItems = menuItems;
         this.dateLastUpdated = dateLastUpdated;
+    }
+
+    //menuItems getter & setter
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+    public void setMenuItems(ArrayList<MenuItem> aMenuItems) {
+        this.menuItems = aMenuItems;
+    }
+
+    //dateLastUpdate getter & setter
+    public Date getDateLastUpdated() {
+        return dateLastUpdated;
+    }
+    public void setDateLastUpdated(Date aDateLastUpdated) {
+        this.dateLastUpdated = aDateLastUpdated;
     }
 }
